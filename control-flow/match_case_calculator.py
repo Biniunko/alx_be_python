ee92ffef1fe530d85a19df8b949802e5c4183c38
Biniunko <106870130+Biniunko@users.noreply.github.com>
2024-09-22 13:20:53 +0300

    added match_case_calculaotr.py

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,23 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+operator = input("Choose the operation (+, -, *, /): ")
+valid_operation = True
+
+match operator:
+    case "+":
+        print(num1 + num2)
+    case "-":
+        print(num1 - num2)
+    case "*":
+        print(num1 * num2)
+    case "/":
+        if num2 != 0:
+            print(num1 / num2)
+        else:
+            print("Cannot divide by zero!")
+            valid_operation = False
+    case _:
+        print("invalid operation")
+
+if valid_operation:
+    print(f"The result is {operator}.")
