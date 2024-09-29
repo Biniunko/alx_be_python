0e69e52b15badbc97926ec70e2c198fce86a4f90
Biniunko <106870130+Biniunko@users.noreply.github.com>
2024-09-29 21:45:26 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,13 +1,13 @@
-def perform_operation(num1 , num2, operation):
-    if operation=="add":
-        return num1+num2
-    elif operation=="subtract":
-        return num1-num2
-    elif operation=="multiply":
-        return num1*num2
-    elif operation=="divide":
-        if num2==0:
-            print("Division by zero is undefined")
+def perform_operation(num1 , num2 , operation):
+    if operation == 'add':
+        return num1 + num2
+    elif operation == 'subtract':
+        return num1 - num2
+    elif operation == 'multiply':
+        return num1 * num2
+    elif operation == 'divide':
+        if num2 == 0:
+            return "Error: Division by zero"
         return num1/num2
     else:
-        return ValueError(f"Invalid operation: {operation}")
+        return "Invalid operation"
