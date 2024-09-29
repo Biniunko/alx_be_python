764f30d85bea3fbd0d3eb0afd76d92f9b4b3d8ec
Biniunko <106870130+Biniunko@users.noreply.github.com>
2024-09-29 21:00:02 +0300

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1,num2,operation):
+def perform_operation(num1 , num2, operation):
     operation = operation.lower()
     if operation == "add":
         return num1 + num2
